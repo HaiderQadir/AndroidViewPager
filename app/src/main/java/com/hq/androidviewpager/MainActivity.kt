ef92f68fea47f4de53f75eed761e04eab7bf5602
Haider Qadir <48542399+HaiderQadir@users.noreply.github.com>
2023-07-11 13:31:37 +0500

    jitpack configured

--- app/src/main/java/com/hq/androidviewpager/MainActivity.kt
+++ app/src/main/java/com/hq/androidviewpager/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.hq.androidviewpager
 
 import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.hq.androidviewpagerlib.fragments.ViewPagerFragment
 
 /** @author HaiderQadir **/
 
 class MainActivity : AppCompatActivity() {
-
-    private var mFrameLayout: FrameLayout? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mFrameLayout = findViewById<FrameLayout>(R.id.frameLayout)
-        val mFragment = ViewPagerFragment(
-            false,
-            settingsIconVisibility = false,
-            infoIconVisibility = false
-        )
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, mFragment).commit()
     }
 }
